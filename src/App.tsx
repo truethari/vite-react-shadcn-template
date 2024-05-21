156b91dffceb0932440a280ce51d4ff25907b2ca
Tharindu N. Madusanka <tharindu.nm@yahoo.com>
2024-05-21 18:55:07 +0530

    init: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <div className="flex flex-col items-center min-h-screen min-w-screen">
         <></>
+        <Button>Click me</Button>
       </div>
     </>
   );
